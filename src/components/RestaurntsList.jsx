fa72dda5ce2f2abedbc04a47c49af0b4fa044ac7
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-10-06 11:35:32 -0400

    Return restaurants of each group

--- src/components/RestaurntsList.jsx
+++ src/components/RestaurntsList.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const RestaurantsList = ({ title, restaurants }) => {
   return (
     <View>
-      <Text style={styles.titleStyle}>{title}</Text>
-      <Text># of restaurants: {restaurants.length}</Text>
+      <Text style={styles.titleStyle}>
+        {title} ({restaurants.length})
+      </Text>
+      <FlatList
+        horizontal
+        data={restaurants}
+        keyExtractor={(restaurant) => restaurant.id}
+        renderItem={({ item }) => {
+          return <Text style={{ marginRight: 25 }}>{item.name}</Text>;
+        }}
+      />
     </View>
   );
 };
